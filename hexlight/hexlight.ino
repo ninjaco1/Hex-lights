c49a1e6b4c782ede712d296c9c345806efccee8d
ninjaco1 <nguyean6@users.noreply.github.com>
2022-03-11 23:20:04 -0800

    basic functions

--- hexlight/hexlight.ino
+++ hexlight/hexlight.ino
@@ -8,10 +8,37 @@
 Theme theme; // create object themes
 
 
-void setup(){
+// functions
 
+//******************************************************************************
+//                            chk_buttons
+// Checks the state of the button number passed to it. It shifts in ones till
+// the button is pushed. Function returns a 1 only once per debounced button
+// push so a debounce and toggle function can be implemented at the same time.
+// Adapted to check all buttons from Ganssel's "Guide to Debouncing"
+// Expects active low pushbuttons on PINA port.  Debounce time is determined by
+// external loop delay times 12. Looks for rising edge on button so held actions
+// have no unexpected effect.
+//******************************************************************************
+uint8_t chk_buttons(uint8_t pin)
+{
+    static uint16_t state = 0;                          // holds state of debounce reading
+    state = (state << 1) | (digitalRead(pin)) | 0xE000; // mask on PINA button
+    if (state == 0xEFFF)                                // compares to rising edge value (so holding doesn't stack)
+        return 1;
+
+    // if not true for 12 times, return false
+    return 0;
+}
+
+void setup(){
+    // pinMode for buttons
+    pinMode(4, INPUT); 
 }
 
 void loop(){
 
+    if (chk_buttons(4)){
+        // check this push button
+    }
 }
